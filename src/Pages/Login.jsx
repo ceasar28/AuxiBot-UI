3426e82e90a5cc59bc93cf7b349c9d140774613c
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-12-12 00:53:14 +0100

    web5 auth

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Web5 } from "@web5/api";
+import React, { useState, useEffect } from "react";
+import { Web5 } from "@web5/api/browser";
 import { Link } from "react-router-dom";
 import { BsEye, BsEyeSlash } from "react-icons/bs";
 import loginImage from "../assets/amico.png";
@@ -10,24 +10,23 @@ const Login = () => {
   const [web5, setWeb5] = useState(null);
   const [myDid, setMyDid] = useState(null);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleFirstNameChange = (e) => {
+    setfirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setlastName(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
-
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
   };
 
   const [message, setMessage] = useState(false);
@@ -39,8 +38,9 @@ const Login = () => {
       setMyDid(did);
 
       if (web5 && did) {
-        await configureProtocol(web5, did);
-        await fetchDings(web5, did);
+        console.log(did);
+        // await configureProtocol(web5, did);
+        // await fetchDings(web5, did);
       }
     };
     initWeb5();
@@ -48,11 +48,11 @@ const Login = () => {
 
   useEffect(() => {
     if (!web5 || !myDid) return;
-    const intervalId = setInterval(async () => {
-      await fetchDings(web5, myDid);
-    }, 2000);
+    // const intervalId = setInterval(async () => {
+    //   await fetchDings(web5, myDid);
+    // }, 2000);
 
-    return () => clearInterval(intervalId);
+    // return () => clearInterval(intervalId);
   }, [web5, myDid]);
   return (
     <div className="w-full min-h-full flex justify-between bg-white">
@@ -61,24 +61,37 @@ const Login = () => {
       </div>
       <div className="w-[60vw] min-h-[100vh] rounded-tl-[40px] rounded-bl-[40px] bg-white ms:absolute right-0 flex flex-col justify-center items-center m-auto">
         <div className="w-[354px] text-center text-black text-[20px] sm:text-[30px] font-semibold font-['Inter']">
-          Welcome Back
+          Hello! Welcome
         </div>
-
-        <button className="w-full h-[50px] ss:w-[475px] mt-[1rem] input__tag border-2 hover:btn-hover font-Sora text-[16px] sm:text-[20px] rounded-[10px] border-neutral-900 border-opacity-75 justify-around flex items-center mb-[14px] m-auto p-1">
-          <img src={googleImg} alt="Logo" className="w-6 h-6" />
-          <div className="text-center text-neutral-900 text-opacity-95 font-medium font-['Inter']">
-            Sign Up with Google
-          </div>
-        </button>
-        <div className="w-[58px] h-[39px] text-center text-black text-[25px] font-semibold font-['Inter']">
-          OR
-        </div>
+        <div className="w-[58px] h-[39px] text-center text-black text-[25px] font-semibold font-['Inter']"></div>
 
         <form
           className="flex flex-col w-full ss:w-[475px]"
           onSubmit={handleSubmit}
         >
           {/* Email Input */}
+          <div>
+            <p className="text-[16px] font-Sora font-medium mb-[14px]">
+              First Name
+            </p>
+            <Input
+              type={"text"}
+              placeholder="First Name"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </div>
+          <div>
+            <p className="text-[16px] font-Sora font-medium mb-[14px]">
+              Last Name
+            </p>
+            <Input
+              type={"text"}
+              placeholder="Last NAME"
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </div>
           <div>
             <p className="text-[16px] font-Sora font-medium mb-[14px]">Email</p>
             <Input
@@ -89,56 +102,20 @@ const Login = () => {
             />
           </div>
 
-          {/* Password Input */}
-          <div>
-            <p className="text-[16px] font-Sora font-medium mb-[14px]">
-              Password
-            </p>
-            <div className="relative w-full">
-              <Input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter your Password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              {/* Toggle password visibility button */}
-              <button
-                type="button"
-                onClick={togglePasswordVisibility}
-                className="password-toggle-button text-xl absolute top-[50%] right-[1rem] transform translate-y-[-90%]"
-              >
-                {showPassword ? <BsEye /> : <BsEyeSlash />}
-              </button>
-            </div>
-          </div>
-
           <Button
             onClick={handleSubmit}
             className={
               "w-full h-[50px] mt-[1rem] input__tag border-2 border-primary-600 rounded-md hover:btn-hover font-Sora text-[16px] xs:text-[16px] bg-primary-600 text-white"
             }
           >
-            Log In
+            Connect to Web5
           </Button>
-          <Link to="#">
-            <p className="text-[16px] font-Sora mt-2 font-medium mb-[14px]">
-              Forgot Password?
-            </p>
-          </Link>
 
           {message && (
             <p className="mt-[0.5rem] text-center text-[19px] font-semibold">
               {message}
             </p>
           )}
-          <h2 className="mt-[1rem] text-center text-[16px] text-primary-400 tracker-medium font-semibold font-Work-Sans">
-            Don&apos;t Have Account?{" "}
-            <Link to="/SignUp">
-              <span className="font-bold hover:underline cursor-pointer font-Sora">
-                Register
-              </span>
-            </Link>
-          </h2>
         </form>
       </div>
     </div>
