bec7700b38f1f43a4de830c17c40b3274b42b303
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-12-12 13:58:00 +0100

    connecting to web5

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Web5 } from "@web5/api/browser";
 import { Link } from "react-router-dom";
 import { BsEye, BsEyeSlash } from "react-icons/bs";
@@ -6,7 +7,10 @@ import loginImage from "../assets/amico.png";
 import googleImg from "../assets/devicon_google.jpg";
 import Input from "../Components/Input";
 import Button from "../Components/Button";
+import { toast } from "react-toastify";
+// import auxiBotProtocol from "../utils/protocol.json";
 const Login = () => {
+  const navigate = useNavigate();
   const [web5, setWeb5] = useState(null);
   const [myDid, setMyDid] = useState(null);
   const [email, setEmail] = useState("");
@@ -25,11 +29,140 @@ const Login = () => {
     setlastName(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    let loading = toast.loading("loading");
+    // to check available protocols
+    // const { protocols } = await web5.dwn.protocols.query({
+    //   message: {
+    //     filter: {
+    //       protocol: "https://didcomm.org/auxi-bot-protocol",
+    //     },
+    //   },
+    // });
+    // console.log("procols available :", protocols);
+
+    const user = constructUserProfile();
+    console.log(user);
+    const record = await writeToDwn(user);
+    console.log(record);
+    if (record) {
+      const { status } = await record.send(myDid); // send the record to the user's remote DWeb Nodes
+      console.log(status);
+      console.log(await record.data.text());
+      console.log("Send record status", record);
+      toast.update(loading, {
+        render: `Succesfully connected`,
+        type: "success",
+        isLoading: false,
+        autoClose: 3000, // Optional: Close the toast after 3 seconds
+      });
+      return navigate("/");
+    } else {
+      console.log("no record");
+    }
+
+    // await fetchDings(web5, myDid);
+    // setNoteValue("");
   };
 
   const [message, setMessage] = useState(false);
+
+  const createProtocolDefinition = () => {
+    const auxiBotProtocolDefinition = {
+      protocol: "https://didcomm.org/auxi-bot-protocol",
+      published: true,
+      types: {
+        user: {
+          schema: "https://didcomm.org/auxi-bot-protocol/schemas/user.json",
+          dataFormats: ["application/json"],
+        },
+        auxi: {
+          schema: "https://didcomm.org/auxi-bot-protocol/schemas/auxi.json",
+          dataFormats: ["application/json"],
+        },
+        todo: {
+          schema: "https://didcomm.org/auxi-bot-protocol/schemas/todo.json",
+          dataFormats: ["application/json"],
+        },
+      },
+      structure: {
+        user: {
+          $actions: [
+            {
+              who: "anyone",
+              can: "read",
+            },
+            {
+              who: "anyone",
+              can: "write",
+            },
+          ],
+          todo: {
+            $actions: [
+              {
+                who: "anyone",
+                can: "read",
+              },
+              {
+                who: "anyone",
+                can: "write",
+              },
+            ],
+          },
+          auxi: {
+            $actions: [
+              { who: "anyone", can: "write" },
+              { who: "anyone", can: "read" },
+              { who: "anyone", can: "read" },
+            ],
+          },
+        },
+      },
+    };
+    return auxiBotProtocolDefinition;
+  };
+
+  const queryForProtocol = async (web5) => {
+    return await web5.dwn.protocols.query({
+      message: {
+        filter: {
+          protocol: "https://didcomm.org/auxi-bot-protocol",
+        },
+      },
+    });
+  };
+
+  const installProtocolLocally = async (web5, protocolDefinition) => {
+    return await web5.dwn.protocols.configure({
+      message: {
+        definition: protocolDefinition,
+      },
+    });
+  };
+
+  const configureProtocol = async (web5, did) => {
+    const protocolDefinition = await createProtocolDefinition();
+
+    const { protocols: localProtocol, status: localProtocolStatus } =
+      await queryForProtocol(web5);
+    console.log({ localProtocol, localProtocolStatus });
+    if (localProtocolStatus.code !== 200 || localProtocol.length === 0) {
+      const { protocol, status } = await installProtocolLocally(
+        web5,
+        protocolDefinition
+      );
+      console.log("Protocol installed locally", protocol, status);
+
+      const { status: configureRemoteStatus } = await protocol.send(did);
+      console.log(
+        "Did the protocol install on the remote DWN?",
+        configureRemoteStatus
+      );
+    } else {
+      console.log("Protocol already installed");
+    }
+  };
 
   useEffect(() => {
     const initWeb5 = async () => {
@@ -40,7 +173,7 @@ const Login = () => {
       if (web5 && did) {
         console.log(web5);
         console.log(did);
-        // await configureProtocol(web5, did);
+        await configureProtocol(web5, did);
         // await fetchDings(web5, did);
       }
     };
@@ -55,6 +188,34 @@ const Login = () => {
 
     // return () => clearInterval(intervalId);
   }, [web5, myDid]);
+
+  const constructUserProfile = () => {
+    const currentDate = new Date().toLocaleDateString();
+    const currentTime = new Date().toLocaleTimeString();
+    const user = {
+      userDid: myDid,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      timestampWritten: `${currentDate} ${currentTime}`,
+    };
+    return user;
+  };
+
+  const writeToDwn = async (user) => {
+    const { record } = await web5.dwn.records.create({
+      data: user,
+      message: {
+        protocol: "https://didcomm.org/auxi-bot-protocol",
+        protocolPath: "user",
+        schema: "https://didcomm.org/auxi-bot-protocol/schemas/user.json",
+        dataFormat: "application/json",
+      },
+    });
+
+    return record;
+  };
+
   return (
     <div className="w-full min-h-full flex justify-between bg-white">
       <div className="w-[45vw] min-h-[100vh] bg-violet-900 hidden ms:flex justify-center items-center">
