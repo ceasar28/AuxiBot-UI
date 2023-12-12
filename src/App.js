9ef8381ccc595db910ef4a302e5622bd1b264fb4
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-12-11 19:12:11 -0700

    return

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import Logo from './Components/Logo';
 import Reminder from './screen/Reminder.jsx'
 import Task from './screen/Task.jsx'
 
 function App() {
   return (
     <Router>
+      <Logo />
         <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
           <Route path='/Reminder' element={<Reminder/>} />
           <Route path='/Task' element={<Task/>} />
         </Routes>
