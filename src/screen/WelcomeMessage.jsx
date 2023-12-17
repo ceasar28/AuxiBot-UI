927f2520832160142dce5cfd35e144943931e5e3
em4michael <aniehegc@gmail.com>
2023-12-17 01:41:30 -0500

    font changes

--- src/screen/WelcomeMessage.jsx
+++ src/screen/WelcomeMessage.jsx
@@ -6,19 +6,19 @@ import TaskModal from "./TaskModal";
 const WelcomeMessage = ({ fName }) => {
   return (
     
-    <div className="WelcomeMessage fixed bottom-[5rem] flex flex-col items-start justify-center w-[75vw] sm:w-[60vw] m-auto">
-      <div className="Auxibot flex mt-0 text-start text-violet-900 text-xl font-bold font-['Inter'] mb-0">
+    <div className="WelcomeMessage fixed bottom-[5rem] flex flex-col items-start justify-center w-[90vw] sm:w-[60vw] m-auto">
+      <div className="Auxibot flex mt-0 text-start text-violet-900 text-xl font-bold font-Sora mb-0">
                 <img src={auxibot} alt="Logo" className="w-6 h-6" />
                 AuxiBot
             </div>
-      <div className="WelcomeBelieve text-start text-gray-900 text-opacity-75 text-xl font-normal font-['Inter']">
+      <div className="WelcomeBelieve text-start text-gray-900 text-opacity-75 text-xl font-normal font-Sora">
         Welcome {fName}
       </div>
       <div className="Group20 flex flex-wrap gap-5">
       <Link to="/Reminder">
         <div className="Group17 cursor-pointer">
         
-          <div className="SetReminder w-[200px] [50px] text-center text-black text-xl font-normal font-['Inter'] bg-violet-900 bg-opacity-30 rounded-[20px] flex items-center justify-center m-auto cursor-pointe p-2 cursor-pointer">
+          <div className="SetReminder w-[200px] [50px] text-center text-black text-xl font-normal font-Sora bg-violet-900 bg-opacity-30 rounded-[20px] flex items-center justify-center m-auto cursor-pointe p-2 cursor-pointer">
             Set Reminder
           </div>
           
@@ -26,7 +26,7 @@ const WelcomeMessage = ({ fName }) => {
         </Link>
         <Link to="/Task">
         <div className="Group18">
-          <div className="MakeAList w-[200px] [50px] text-center text-black text-xl font-normal font-['Inter'] bg-violet-900 bg-opacity-30 rounded-[20px] flex items-center justify-center m-auto cursor-pointer p-2">
+          <div className="MakeAList w-[200px] [50px] text-center text-black text-xl font-normal font-Sora bg-violet-900 bg-opacity-30 rounded-[20px] flex items-center justify-center m-auto cursor-pointer p-2">
             Make a list
           </div>
         </div>
